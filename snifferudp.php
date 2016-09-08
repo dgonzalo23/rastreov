d69add74859dace4db79d10b137c17023299af0f
dgonzalo23 <dgonzalo@uninorte.edu.co>
2016-09-08 06:56:08 -0500

    Update snifferudp.php

--- snifferudp.php
+++ snifferudp.php
@@ -15,19 +15,16 @@ $pesos = explode('-',$mensaje_div[1]);
 
 $s1=strpos($mensaje,"#");    $s2=strpos($mensaje,"@");  $lon=strlen($mensaje);
 
-if (!empty($s1) && !empty($s2) && $lon<77 && $lon>66){
 
 $conexion = @mysql_connect("localhost","root","uninorte") or die("No se encontro el servidor");
 mysql_select_db("rastreovehicular",$conexion) or die ("No se encontro base de datos");
 
-$lat=strpos($mensaje_div[2],"+");    $lng=strpos($mensaje_div[2],"-");
-$id=strpos($mensaje_div[2],"ID=");    $time=substr($mensaje_div[2],$lat-10,10);
+$lat=strpos($mensaje,"+");    $lng=strpos($mensaje,"-");
+$id=strpos($mensaje,"ID=");    $time=substr($mensaje,$lat-10,10);
 $weeksTosecond=substr($time,0,4)*7*24*60*60;    $daysToseconds=substr($time,4,1)*24*60*60;    $seconds=substr($time,5,5);
 $tiempogps=$weeksTosecond+$daysToseconds+$seconds+315964800;
-$latitud=substr($mensaje_div[2],$lat+1,2).'.'.substr($mensaje_div[2],$lat+3,5);
-$longitud='-'.substr($mensaje_div[2],$lng+2,2).'.'.substr($mensaje_div[2],$lng+4,5);
-$usuario=substr($mensaje_div[2],$id+3,3);
-$placa=substr($mensaje_div[2],$id+6,6);
+$latitud=substr($mensaje,$lat+1,2).'.'.substr($mensaje,$lat+3,5);
+$longitud='-'.substr($mensaje,$lng+2,2).'.'.substr($mensaje,$lng+4,5);
 $fecha=date('Y-m-d H:i:s', $tiempogps);
 $fecha1 = substr($fecha,0,10);
 $fecha2 = substr($fecha,10,18);
